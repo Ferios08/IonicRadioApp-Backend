2adfa3c67055823e1c1183010c4135d426ca2ccf
ferios08 <chb.firas@gmail.com>
2019-09-03 11:49:54 +0100

    made minor changes

--- helpers/database.js
+++ helpers/database.js
@@ -1,7 +1,6 @@
 var mysql = require('mysql')
-
-var db_name = "Backend",
-// process.env.NODE_ENV === 'production' && 'Backend' || 'Backend-dev',
+var db_name =  'Backend-'+ (process.env.NODE_ENV || 'dev'),
+//process.env.NODE_ENV === 'production' && 'Backend' || 'Backend-dev',
     db_host = '13.68.196.225',
     //db_host = 'localhost',
     db_pass = 'ferios08',
